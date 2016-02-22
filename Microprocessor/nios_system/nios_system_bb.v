f1da00e658b2011a804358422946ceb021ceb050
bbednarski9 <bbednarski9@gmail.com>
2016-02-22 15:49:38 -0800

    some microprocessor updates
    Will branch off and do pin assignments from here.

--- Microprocessor/nios_system/nios_system_bb.v
+++ Microprocessor/nios_system/nios_system_bb.v
@@ -2,9 +2,21 @@
 module nios_system (
 	reset_reset_n,
 	ledr_export,
-	clk_clk);	
+	paralleltoprocessor_export,
+	clk_clk,
+	parallelfromprocessor_export,
+	transmitenable_export,
+	charactersent_export,
+	load_export,
+	characterreceived_export);	
 
 	input		reset_reset_n;
 	output	[7:0]	ledr_export;
+	input	[7:0]	paralleltoprocessor_export;
 	input		clk_clk;
+	output	[7:0]	parallelfromprocessor_export;
+	output		transmitenable_export;
+	output		charactersent_export;
+	output		load_export;
+	input		characterreceived_export;
 endmodule
